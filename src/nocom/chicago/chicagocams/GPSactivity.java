f2cbb236e855904e310ffe55ed3d3dcf627a67a9
Andrew <andrewtspalding@gmail.com>
2014-03-06 19:40:42 -0600

    first comment

---
+++ src/nocom/chicago/chicagocams/GPSactivity.java
@@ -0,0 +1,188 @@
+package nocom.chicago.chicagocams;
+
+import java.util.Locale;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.TextView;
+import android.widget.Toast; 
+import android.widget.ImageView;
+
+
+
+public class GPSactivity extends Activity implements LocationListener, OnInitListener {
+	public LocationManager lm;
+	public double lat,lon;
+	public float speed;
+	
+	public Model myModel;
+	
+	public TextToSpeech tts;
+	public boolean said;
+	
+	public TextView locationLat, locationLon, warningTxt, speedTxt;
+	public ImageView warningImg;
+	
+	public Canvas canvas;
+	public float x,y,radius;
+	
+	//Lots of initialization. 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		//init GPS
+		super.onCreate(savedInstanceState);
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);	    
+		
+		//init layout.
+		setContentView(R.layout.activity_gpsactivity);
+		
+		locationLat = (TextView) findViewById(R.id.locLat);
+		locationLon = (TextView) findViewById(R.id.locLon);
+		
+		speedTxt = (TextView) findViewById(R.id.speedTxt);
+		warningTxt = (TextView) findViewById(R.id.warnTxt);
+		
+		warningImg = (ImageView) findViewById(R.id.warnImg);
+		
+		//init status circle. 
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+		int width = metrics.widthPixels;
+		int height = metrics.heightPixels;
+		
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		canvas = new Canvas(bitmap);
+		warningImg.setImageBitmap(bitmap);
+
+		Paint paint = new Paint();
+		paint.setColor(Color.GREEN);
+		x = width/2;
+		y = height/2;
+		radius = 200;
+		
+		canvas.drawCircle(x, y, radius, paint);
+		
+		//init model.
+		myModel = new Model(this.getApplicationContext().getAssets());
+		
+		//init text to speech.
+		tts = new TextToSpeech(this, this);
+		said = false; 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.gpsactivity, menu);
+		return true;
+	}
+	
+
+	 @Override
+	 public void onLocationChanged(Location loc){	
+		lat = loc.getLatitude();
+		lon = loc.getLongitude();
+		
+		float msTomph = (float)2.237;
+		speed = loc.getSpeed() * msTomph;
+		speedTxt.setText(String.valueOf(speed) + " mph");
+		
+		locationLat.setText(String.valueOf(lat));
+		locationLon.setText(String.valueOf(lon));
+	     
+	     
+		Location currentLocation = new Location("Current");
+		currentLocation.setLatitude(lat);
+		currentLocation.setLongitude(lon);
+	    
+		myModel.sortByLocation(currentLocation);   
+	     
+	     
+	    if(currentLocation.distanceTo(myModel.getNearest()) < 225){
+			if(said != true){
+	    		String seriousWarning = "You are approximately one block from a speed camera. You are traveling at "
+	    	 		+ Math.round(speed) + " miles per hour.";
+	    	
+	    		tts.speak(seriousWarning, TextToSpeech.QUEUE_ADD, null);
+	    		
+	    		said = true;
+	    	}
+	    	
+	    	warningTxt.setTextColor(Color.RED);
+	    	
+	    	Paint paintWarn = new Paint();
+	    	paintWarn.setColor(Color.RED);
+	 		
+	 		canvas.drawCircle(x, y, radius, paintWarn);
+	    }
+	    
+	    else{
+	    	warningTxt.setTextColor(Color.WHITE);
+	    	 
+	    	Paint paint = new Paint();
+	 		paint.setColor(Color.GREEN);
+	 		
+	 		canvas.drawCircle(x, y, radius, paint);
+	 		
+	 		said = false;
+	    }
+	    
+	    warningTxt.setText(myModel.getNearest().getProvider() + "\nTo nearest speed camera: " + String.valueOf(currentLocation.distanceTo(myModel.getNearest())));
+
+	 }
+	 
+	@Override
+	public void onInit(int code) {
+		if(code==TextToSpeech.SUCCESS){
+			tts.setLanguage(Locale.getDefault());
+		}
+		else{
+			tts = null;
+			Toast.makeText(this, "Failed to initialize TTS engine.", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		if(tts!=null){
+			tts.stop();
+			tts.shutdown();
+		}
+		if(lm!=null){
+			lm.removeUpdates(this);
+		}
+		
+		super.onDestroy();
+	}
+
+	@Override
+	public void onProviderDisabled(String provider){
+		Toast.makeText(getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT ).show();
+	}
+
+	 @Override
+	 public void onProviderEnabled(String provider){
+		Toast.makeText(getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+	 }
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras){
+
+	}
+	 
+}
---
+++ src/nocom/chicago/chicagocams/Model.java
@@ -0,0 +1,115 @@
+package nocom.chicago.chicagocams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import android.content.res.AssetManager;
+import android.location.Location;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Model {
+	
+	private Location nearest;
+	private ArrayList<Location> cameras;
+	
+	private AssetManager asset;
+
+	public Model(AssetManager appAssets) {
+		asset = appAssets;
+		
+		cameras = new ArrayList<Location>();
+		
+		try {
+			this.loadLocationsFromJSON();
+		} catch (JSONException e) {
+			 e.printStackTrace();
+		}
+		
+	}
+	
+	public Location getNearest(){
+		nearest = cameras.get(0);
+		return nearest;
+	}
+	
+	//Sort by location nearest to your current location.
+	public void sortByLocation(Location currentLocation){
+		boolean swapped = true;
+		
+		while(swapped){
+			swapped = false;
+			
+			for(int i = 1; i < cameras.size(); i++){
+				if(cameras.get(i-1).distanceTo(currentLocation) > cameras.get(i).distanceTo(currentLocation)){
+					Location tmp = cameras.get(i);
+					cameras.set(i, cameras.get(i-1));
+					cameras.set(i-1, tmp);
+					swapped = true;
+				}
+			}
+		}
+	}
+	
+	//Returns JSON in String format.
+	private String loadJSONFromAsset(){
+		String json = null;
+		
+		try{
+			InputStream is = asset.open("speedcams");
+			int size = is.available();
+			byte[] buffer = new byte[size];
+			
+			is.read(buffer);
+			is.close();
+			json = new String(buffer);
+		}
+		catch(IOException ex){
+			ex.printStackTrace();
+			return null;
+		}
+		
+		return json;
+	}
+	
+	//Loads all locations into an ArrayList. See block comment for bizare behavior.
+	private void loadLocationsFromJSON() throws JSONException{
+		JSONArray toParse = new JSONArray(this.loadJSONFromAsset());
+		JSONObject current = null;
+		
+		for(int i = 0; i < toParse.length(); i++){
+			Location currentLoc;
+			String camera; 
+			double lat,lon;
+			
+			try{
+				current = toParse.getJSONObject(i);
+				
+				camera = current.getString("fullname");
+				lat = current.getDouble("lat");
+				lon = current.getDouble("lon");
+				
+				currentLoc = new Location(camera);
+				/*
+				 * This is super bizarre. Either the city of Chicago got it mixed up 
+				 * or the Android API has latitude and longitude mixed up.
+				 */
+				currentLoc.setLatitude(lon);
+				currentLoc.setLongitude(lat);
+				
+				cameras.add(currentLoc);
+
+			} 
+			catch (JSONException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	 
+
+}
